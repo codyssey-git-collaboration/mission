04eee6cf75cdc09277fc4aad1968b21793d6b7d1
whitecy01 <whitecy01@naver.com>
2026-07-26 17:42:46 +0900

    Revert "feat: add discount util (wrong rate 1.5)"
    This reverts commit bb0183d3ec0e5cba6aeba47499a4e9d582fee4f5.

--- src/discount.py
+++
@@ -1,9 +0,0 @@
-"""할인율 계산 유틸 (실습용)."""
-
-# 잘못된 값: 할인율이 1.5(150%)로 잘못 설정됨
-DISCOUNT_RATE = 1.5
-
-
-def apply_discount(price: float) -> float:
-    """할인 적용 가격을 반환한다."""
-    return price * (1 - DISCOUNT_RATE)
